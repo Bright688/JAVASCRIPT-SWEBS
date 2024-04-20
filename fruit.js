c70990c5c816db9271d73d52e064f38b41f5f95f
Bright686 <akaraiwebright0@gmail.com>
2024-04-20 13:21:16 +0000

    javascript codes containing different method

---
+++ fruit.js
@@ -0,0 +1,31 @@
+let fruit = ['apples', 'bananas', 'oranges'];
+let number = [1, 2, 3, 4];
+let mixedArr = ['hello', 123, true, {first_name: 'john'}, ['coding']];
+console.log(mixedArr);
+console.log(mixedArr[3]);
+console.log(fruit.constructor);
+//Array method
+let newarr = fruit.concat(number);
+console.log(newarr);
+//push method
+fruit.push("water Melon", "Guava");
+//removes the last element
+fruit.pop();
+fruit.push("Mango");
+//remove the first element
+fruit.shift()
+//add to the beginning of the array
+fruit.unshift("Guava", "water melon");
+//splice. This is used to change the content of the array by removing existing elements and or adding new elements in place.
+//it takes three parameter
+fruit.splice(2, 4, "Mango");
+fruit.push("Apple");
+
+//slice method. Returns a shallow copy of a portion of an array into a new array object
+let sliceditem = fruit.slice(1, 2);
+console.log(sliceditem);
+
+//include method. Determines whether an array includes a certain value among its elements returning true or false as appropriate
+let presentFruit = fruit.includes("shawasour");
+console.log(presentFruit);
+
